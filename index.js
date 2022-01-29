38d1ae559716830894a871cfeef845c8f1bc1a27
Aditya Sonani <adityasonani19@gmail.com>
2022-01-29 10:22:07 +0530

    Minor bug fixes

--- index.js
+++ index.js
@@ -15,21 +15,38 @@ const MAX_ALLOWED_SIZE = 100 * 1024 * 1024; //100MB
 
 dropZone.addEventListener("dragover", (e)=>{
     e.preventDefault();
-    console.log("dragged");
+    // console.log("dragged");
+    dropZone.classList.add("dragged");
+});
+
+dropZone.addEventListener("dragleave", (e)=>{
+    dropZone.classList.remove("dragged");
 });
 
 dropZone.addEventListener("drop", (e)=>{
     e.preventDefault();
     const files = e.dataTransfer.files;
-    console.log(files);
-    if (files.length) {
-        fileInput.files = files;
-        uploadFile();
+    // console.log(files);
+    if (files.length === 1) {
+        if (files[0].size < MAX_ALLOWED_SIZE) {
+            fileInput.files = files;
+            uploadFile();
+        } else {
+            showToast("Max file size is 100MB!");
+        }
+    } else if (files.length > 1) {
+        showToast("Please upload 1 file at a time!");
     }
+    dropZone.classList.remove("dragged");
 }); 
 
 fileInput.addEventListener("change", ()=>{
-    uploadFile();
+    if (fileInput.files[0].size < MAX_ALLOWED_SIZE) {
+        uploadFile();
+    } else {
+        showToast("Max file size is 100MB!");
+        fileInput.value = "";
+    }
 });
 
 browsebtn.addEventListener("click", ()=>{
@@ -42,43 +59,60 @@ copyLink.addEventListener("click", ()=>{
     showToast("Copied To Clipboard");
 });
 
+fileUrl.addEventListener("click", () => {
+    fileUrl.select();
+});
+
+
 const uploadFile = ()=>{
-    if (fileInput.files.length > 1) {
-        fileInput.value = "";
-        showToast("Please upload 1 file at a time.");
-        return;
-    }
-    const file = fileInput.files[0];
+    const file = fileInput.files;
     
-    if (file.size > MAX_ALLOWED_SIZE) {
-        showToast("Please upload files less than 100MB size");
-        fileInput.value = "";
-        return;
-    }
+    const formData = new FormData();
+    formData.append("myfile", file[0]);
     
     progressContainer.style.display = "block";
-    const formData = new FormData();
-    formData.append("myfile", file);
 
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
+
+    xhr.upload.onprogress = function (event) {
+        let percent = Math.round((100 * event.loaded) / event.total);
+        bgProgress.style.width = `${percent}%`;
+    }
+
+    xhr.upload.onerror = function () {
+        showToast(`Error in upload: ${xhr.status}`);
+        fileInput.value = "";
+    }
+
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.response);
-            showLink(JSON.parse(xhr.response));
+            onFileUploadSuccess(xhr.responseText);
         }
     };
-
+    /*
     xhr.upload.onprogress = updateProgress;
 
     xhr.upload.onerror = ()=>{
         fileInput.value = "";
         showToast(`Error in upload: ${xhr.statusText}`);
     };
+    */
 
     xhr.open("POST", uploadUrl);
     xhr.send(formData);
 
 };
+
+const onFileUploadSuccess = (res) => {
+    fileInput.value = "";
+    progressContainer.style.display = "none";
+
+    const {file : url} = JSON.parse(res);
+    console.log(url);
+    sharingContainer.style.display = "block";
+    fileUrl.value = url;
+    bgProgress.style.width = `0%`;
+}
 
 const updateProgress = (e) => {
     const percent = Math.round((e.loaded / e.total) * 100);
@@ -96,10 +130,12 @@ const showLink = ({file})=>{
 
 let toastTime;
 const showToast = (msg)=>{
-    toast.innerText = msg;
-    toast.style.transform = "translate(-50%, 0)";
     clearTimeout(toastTime);
+    toast.innerText = msg;
+    toast.classList.add("show");
+    // toast.style.transform = "translate(-50%, 0)";
     toastTime = setTimeout(()=>{
-        toast.style.transform = "translate(-50%, 60px)";
+        // toast.style.transform = "translate(-50%, 60px)";
+        toast.classList.remove("show");
     }, 2000);
 };
